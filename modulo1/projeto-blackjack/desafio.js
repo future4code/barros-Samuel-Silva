f0750066f0e5a32e7e1b84a5e383d5b7ca0e6a3d
Samuel <samuca010@gmail.com>
2022-05-05 18:15:54 -0300

    Em andamento Projeto Blackjack

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,51 +11,55 @@
  * 
  */
 
-   
-
     let carta4 = comprarCarta()
     let carta1 = comprarCarta() 
     let carta2 = comprarCarta()
     let carta3 = comprarCarta()
-    let pontosUsuario = carta1.valor + carta2.valor
+    let pontosUsuario = [carta1.valor + carta2.valor]
     let pontosComput =  carta3.valor + carta4.valor
+    let cartasUsuario = [carta1.texto,carta2.texto]
+    let cartasComput = [carta3.texto,carta4.texto]
+    let novaCarta = []
 
-   // Abertura do jogo
-console.log('♣️ ♥️ ♠️ ♦️   BOAS VINDAS AO JOGO DE BLACKJACK!  ♣️ ♥️ ♠️ ♦️')
-if(confirm("Deseja inciar uma nova rodada?")) {
-      
+
+   // Abertura do jogo ---------------------------------------------------------------------------------------------------
+      console.log('♣️ ♥️ ♠️ ♦️   BOAS VINDAS AO JOGO DE BLACKJACK!  ♣️ ♥️ ♠️ ♦️')
+      if(confirm("Deseja inciar uma nova rodada?")) {
       comprarCarta() 
-   // Teste do 2 Ases
-      if (pontosUsuario !== 22 && pontosComput !== 22) {
-       iniciaJogo()
-      } else {
-       console.log('Dois Ases sorteados, reiniciando rodada')  
-       comprarCarta()
-             }
    
-   // Inicio da rodada   
-
-   confirm(
-      "Suas cartas são: " + carta1.texto + carta2.texto  + ". A carta revelada do computador é " + carta3.texto +
-      "\n"+  // \n faz pular a linha
-      "Deseja comprar mais uma carta?"
-   )
-    
-      function definejogo () {
-         let cartasUsuario = [carta1,carta2]
-         let cartasComput = [carta3,carta4]
-         // console.log(`Suas cartas: ${carta1.texto} ${carta2.texto} /// Pontuação: ${pontosUsuario}`)
-         // console.log(`Cartas do computador: ${carta3.texto} ${carta4.texto} /// Pontuação: ${pontosComput}`) 
-         if (pontosComput === pontosUsuario){
+   // Teste do 2 Ases -----------------------------------------------------------------------------------------------------
+         if (pontosUsuario !== 22 && pontosComput !== 22) {
+         iniciaJogo()
+      }  else {
+         console.log('Dois Ases sorteados, reiniciando rodada')  
+         comprarCarta()
+         }
+   
+   // // Inicio da rodada . Comprar mais cartas ----------------------------------------------------------------------------  
+            function iniciaJogo ()
+            let pontosUsuarioMaisCompra = (pontosUsuario+novaCarta.valor)                        
+            while (pontosUsuarioMaisCompra < 22) {
+               if(confirm("Suas cartas são: " + cartasUsuario  + ". A carta revelada do computador é " + carta3.texto +
+                    "\n"+  // \n faz pular a linha
+                    "Deseja comprar mais uma carta?")) {
+            novaCarta = comprarCarta()
+            cartasUsuario.push(novaCarta.texto)  
+            pontosUsuarioMaisCompra = pontosUsuario + novaCarta.valor
+            }
+            console.log(`Carta comprada: ` + novaCarta.texto)
+            console.log(`Suas cartas:` + cartasUsuario + `/// Pontuação: ${pontosUsuarioMaisCompra}`)
+            console.log(`Cartas do computador: ${carta3.texto} ${carta4.texto} /// Pontuação: ${pontosComput}`)
+         }             
+         }  else if             
+            (pontosComput === pontosUsuario){
             console.log("Empate")
-         } else if
+         }  else if
             (pontosComput > pontosUsuario){
             console.log("O computador ganhou!")
-         } else if
+         }  else if
             (pontosComput < pontosUsuario){
             console.log("Você ganhou!!!")
-            }
-      }    
-    }else {
+         }
+   
       console.log('O jogo acabou!')
-   }
+            
