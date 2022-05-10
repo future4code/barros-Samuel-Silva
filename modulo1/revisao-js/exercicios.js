6c96fdec629e47822b1e52f672be0eb0eb84a457
Samuel <samuca010@gmail.com>
2022-05-09 22:11:21 -0300

    EXERCICIO 5 CONCLUIDO

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -19,26 +19,26 @@
 // retornaArrayInvertido[0,8,23,16,10,15,41,12,13] 
 // ///////// TEM DESAFIO///////////
 
-// EXERCÍCIO 03
-function retornaArrayOrdenado(array) {
-  let arrayOrdenado = array.sort(function(a,b) {
-        return a -b
-    })
-    return arrayOrdenado
-      }
-retornaArrayOrdenado[36,12,56,7,3]]
-/////////// TEM DESAFIO///////////
+// // EXERCÍCIO 03
+// function retornaArrayOrdenado(array) {
+//   let arrayOrdenado = array.sort(function(a,b) {
+//         return a -b
+//     })
+//     return arrayOrdenado
+//       }
+// retornaArrayOrdenado[36,12,56,7,3]]
+// /////////// TEM DESAFIO///////////
 
-//EXERCÍCIO 04
-function retornaNumerosPares(array) {
-  let arrayPares = array.filter((validaPar) => {
-     return validaPar %2 === 0
-  })
-  return arrayPares
-}
-    retornaNumerosPares[1,2,3,4,5,6,7,8]
+// //EXERCÍCIO 04
+// function retornaNumerosPares(array) {
+//   let arrayPares = array.filter((validaPar) => {
+//      return validaPar %2 === 0
+//   })
+//   return arrayPares
+// }
+//     retornaNumerosPares[1,2,3,4,5,6,7,8]
 
-EXERCÍCIO 05
+//EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
  
 }
