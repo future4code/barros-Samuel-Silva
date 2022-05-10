353cc1dcc44f95bdc526086e8d35ef66251d12b1
Samuel <samuca010@gmail.com>
2022-05-09 22:09:43 -0300

    EXERCICIO 2 CONCLUIDO

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -3,32 +3,33 @@
 //    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
 
 
-// EXERCÍCIO 01 
-// function retornaTamanhoArray(array) {
-// let tamanhoDaLista = array.length  
-// return tamanhoDaLista 
-// }
-//  retornaTamanhoArray[3, 2, 1, 4, 7]
+//EXERCÍCIO 01 
+function retornaTamanhoArray(array) {
+let tamanhoDaLista = array.length  
+return tamanhoDaLista 
+}
+ retornaTamanhoArray[3, 2, 1, 4, 7]
+ 
 
-// // EXERCÍCIO 02
-// function retornaArrayInvertido(array) {
-//   let arrayInvertido = array.reverse()
-//   return arrayInvertido
-// }
-// retornaArrayInvertido[0,8,23,16,10,15,41,12,13] 
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+  let arrayInvertido = array.reverse()
+  return arrayInvertido
+}
+retornaArrayInvertido[0,8,23,16,10,15,41,12,13] 
+///////// TEM DESAFIO///////////
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  let arrayOrdenado = array.sort(function(a,b) {
+        return a -b
+    })
+    return arrayOrdenado
+      }
+retornaArrayOrdenado[36,12,56,7,3]]
 /////////// TEM DESAFIO///////////
 
-// // EXERCÍCIO 03
-// function retornaArrayOrdenado(array) {
-//   let arrayOrdenado = array.sort(function(a,b) {
-//         return a -b
-//     })
-//     return arrayOrdenado
-//       }
-// retornaArrayOrdenado[36,12,56,7,3]]
-// /////////// TEM DESAFIO///////////
-
-// EXERCÍCIO 04
+//EXERCÍCIO 04
 function retornaNumerosPares(array) {
   let arrayPares = array.filter((validaPar) => {
      return validaPar %2 === 0
@@ -37,7 +38,7 @@ function retornaNumerosPares(array) {
 }
     retornaNumerosPares[1,2,3,4,5,6,7,8]
 
-// EXERCÍCIO 05
+EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
  
 }
