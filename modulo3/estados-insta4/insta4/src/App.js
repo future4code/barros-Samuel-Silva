68e071ca0735332efa38aa14292c9d6a83d5702b
Samuel <samuca010@gmail.com>
2022-07-05 22:24:37 -0300

    Introduzindo array com React

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import Post from './components/Post/Post';
 import './style.css'
+import { MainContainer, DivContainer } from './style';
 
+const arrayPosts = [
+  { nomeUsuario: "Luciene", fotoUsuario: 'https://picsum.photos/50/50', fotoPost: 'https://picsum.photos/200/150' },
+  { nomeUsuario: "jhony", fotoUsuario:'https://picsum.photos/60/60' , fotoPost:'https://picsum.photos/200/160' },
+  { nomeUsuario: "marcela", fotoUsuario: 'https://picsum.photos/70/70', fotoPost:'https://picsum.photos/200/170'}
+]
+
+const postMap = arrayPosts.map((element, index) => {
+  return (
+    <DivContainer key={index}><Post 
+      nomeUsuario={element.nomeUsuario}
+      fotoUsuario={element.fotoUsuario}
+      fotoPost={element.fotoPost}/>
+    </DivContainer>
+  )
+})
+console.log([arrayPosts]);
 
 function App() {
-return(
-  <div className='MainContainer'>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-          <Post
-            nomeUsuario={'jhony'}
-            fotoUsuario={'https://picsum.photos/50/60'}
-            fotoPost={'https://picsum.photos/200/160'}
-          />
-          <Post
-            nomeUsuario={'leo'}
-            fotoUsuario={'https://picsum.photos/50/70'}
-            fotoPost={'https://picsum.photos/200/170'}
-          />
-  </div>
-)
-
+  return (
+    <MainContainer>
+      <h1>Insta4</h1>
+      <p>{postMap}</p>
+    </MainContainer>
+  )
 }
-
 
 export default App;
