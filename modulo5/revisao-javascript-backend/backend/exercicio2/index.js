3809798597a3eec146fb027c8b67f606a7f36dee
Samuel <samuca010@gmail.com>
2022-10-04 22:26:09 -0300

    Iniciando do exercício 2

--- modulo5/revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio2/index.js
@@ -1,1 +1,21 @@
-console.log("exercício 2");
+console.log("Exercício 2");
+
+const clients = [
+	{ id: 1, nome: "Fulano" },
+	{ id: 2, nome: "Ciclano" },
+	{ id: 3, nome: "Beltrano" },
+	{ id: 4, nome: "Fulana" }
+]
+
+const newClient = (idClient, nameClient) => {
+    if (clients.indexOf(idClient) === -1 ){
+        return "Erro. Paclientsrâmetro inválido! (id já existe)"
+    } else {
+        console.log(idClient)
+        //console.log(nameClient)
+
+    clients.push({id: idClient, nome: nameClient})
+    }
+}
+    newClient( 5, "Zé") 
+console.log(clients)
