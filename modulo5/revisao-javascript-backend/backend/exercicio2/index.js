30034369db3823df2c9c468cb3c380f8c9f223a7
Samuel <samuca010@gmail.com>
2022-10-06 19:59:29 -0300

    Conclusão do exercício 3

--- modulo5/revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio2/index.js
@@ -1,5 +1,7 @@
 console.log("Exercício 2");
 
+console.log("Cadastrar Clientes");
+
 const clients = [
 	{ id: 1, nome: "Fulano" },
 	{ id: 2, nome: "Ciclano" },
@@ -8,14 +10,53 @@ const clients = [
 ]
 
 const newClient = (idClient, nameClient) => {
-    if (clients.indexOf(idClient) === -1 ){
-        return "Erro. Paclientsrâmetro inválido! (id já existe)"
+    if (clients.indexOf(idClient) !== -1) {
+        console.log(`Erro ${clients.indexOf(idClient)}. Parâmetro inválido! (id ${idClient} já existe) `);
     } else {
+        console.log("Cliente cadastrado com sucesso!")   
+        clients.push({id: idClient, nome: nameClient})
         console.log(idClient)
-        //console.log(nameClient)
-
-    clients.push({id: idClient, nome: nameClient})
     }
 }
-    newClient( 5, "Zé") 
+    newClient(1, "Maria") 
 console.log(clients)
+
+
+console.log("Gerador de tabuada");
+
+const calculator = (multiple = 1) => {
+    let total0 = multiple * 0;
+    let total1 = multiple * 1;
+    let total2 = multiple * 2;
+    let total3 = multiple * 3;
+    let total4 = multiple * 4;
+    let total5 = multiple * 5;
+    let total6 = multiple * 6;
+    let total7 = multiple * 7;
+    let total8 = multiple * 8;
+    let total9 = multiple * 9;
+    let total10 = multiple * 10;
+
+    if (typeof multiple !== "number") {
+        return (console.log("Erro. Parâmetro inválido! (deve ser um número)")
+        )
+    } if (multiple >10  || multiple < 1) {
+        return (console.log("Erro. Parâmetro inválido! (número precisa valer entre 1 e 10)")
+        )
+    } else {(typeof multiple === "number") 
+        return  [
+            `${multiple} x 0 = ${total0}`,
+            `${multiple} x 1 = ${total1}`,
+            `${multiple} x 2 = ${total2}`,
+            `${multiple} x 3 = ${total3}`,
+            `${multiple} x 4 = ${total4}`,
+            `${multiple} x 5 = ${total5}`,
+            `${multiple} x 6 = ${total6}`,
+            `${multiple} x 7 = ${total7}`,
+            `${multiple} x 8 = ${total8}`,
+            `${multiple} x 9 = ${total9}`,
+            `${multiple} x 10 = ${total10}`,
+        ]
+    }
+}
+ console.log(calculator())
